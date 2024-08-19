5889e08fbe9ee048e65fd9f7c01d7d7f170acd35
zildanpandarusm <zildanmarginata@gmail.com>
2024-08-19 14:06:47 +0700

    bismillah

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: 'http://localhost:3000',
+    origin: 'https://kajoran-klaten.vercel.app/',
   })
 );
 app.use(cookieParser());
--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
   BASE_URL: 'https://kajoran-klatenselatan-api.vercel.app',
-  IMAGE_BASE_URL: 'http://localhost:3000',
+  IMAGE_BASE_URL: 'https://kajoran-klaten.vercel.app/',
 };
 
 export default config;
