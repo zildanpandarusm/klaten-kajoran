ecdfd2c8a3f2d5b71ed6c23aed357f728c8504ae
zildanpandarusm <zildanmarginata@gmail.com>
2024-08-20 08:44:52 +0700

    yuk

--- frontend/src/components/admincomponents/addnews.js
+++ frontend/src/components/admincomponents/addnews.js
@@ -1,9 +1,8 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../css/AdminStyle.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faPenSquare, faTrashAlt, faCamera, faFileLines } from '@fortawesome/free-solid-svg-icons';
-import { useTable, usePagination, useSortBy, useGlobalFilter } from 'react-table';
 import axios from 'axios';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
@@ -19,6 +18,7 @@ const AddNews = () => {
   const [hapus, setHapus] = useState(false);
   const [file, setFile] = useState('');
   const [descSingkat, setDescSingkat] = useState('');
+  const [isLoading, setIsLoading] = useState(false); // State untuk loading
   const navigate = useNavigate();
 
   const resetVariable = () => {
@@ -32,6 +32,7 @@ const AddNews = () => {
 
   const handleForm = async (e) => {
     e.preventDefault();
+    setIsLoading(true); // Mulai loading
 
     try {
       const formData = new FormData();
@@ -46,6 +47,8 @@ const AddNews = () => {
       navigate(`/admin/berita`);
     } catch (error) {
       // setMsg(error.response.data.msg);
+    } finally {
+      setIsLoading(false); // Selesai loading
     }
   };
 
@@ -56,52 +59,56 @@ const AddNews = () => {
         <p>Atur ketersediaan beritamu!</p>
       </div>
       <div className="konten">
-        <form onSubmit={handleForm}>
-          <div className="formInput">
-            <label htmlFor="title">
-              Judul <span className="wajib">*</span>
-            </label>
-            <input type="text" id="title" placeholder="Masukkan judul" onChange={(e) => setTitle(e.target.value)} required />
-          </div>
-          <div className="formInput">
-            <label htmlFor="foto">
-              Foto <span className="wajib">*</span>
-            </label>
-            <input type="file" id="foto" onChange={(e) => setFile(e.target.files[0])} />
-          </div>
-          <div className="formInput">
-            <label htmlFor="desc">
-              Deskripsi <span className="wajib">*</span>
-            </label>
-            <ReactQuill className="deskBox" theme="snow" onChange={setDesc} />
-          </div>
-          <div className="formInput">
-            <label htmlFor="descSingkat">
-              Deskripsi Singkat <span className="wajib">*</span>
-            </label>
-            <ReactQuill className="deskBox" theme="snow" onChange={setDescSingkat} />
-          </div>
-          <div className="formInput">
-            <label htmlFor="kategori">
-              Kategori <span className="wajib">*</span>
-            </label>
-            <select id="kategori" onChange={(e) => setCategory(e.target.value)}>
-              <option>Pilih Kategori</option>
-              <option value="politik">politik</option>
-              <option value="ekonomi">ekonomi</option>
-              <option value="sosial">sosial</option>
-              <option value="budaya">budaya</option>
-              <option value="teknologi">teknologi</option>
-              <option value="hiburan">hiburan</option>
-            </select>
-          </div>
-          <div className="formButton">
-            <button type="submit">Tambah</button>
-            <button type="button" onClick={() => listNews()}>
-              Batal
-            </button>
-          </div>
-        </form>
+        {isLoading ? (
+          <div className="loading">Loading...</div> // Tanda loading
+        ) : (
+          <form onSubmit={handleForm}>
+            <div className="formInput">
+              <label htmlFor="title">
+                Judul <span className="wajib">*</span>
+              </label>
+              <input type="text" id="title" placeholder="Masukkan judul" onChange={(e) => setTitle(e.target.value)} required />
+            </div>
+            <div className="formInput">
+              <label htmlFor="foto">
+                Foto <span className="wajib">*</span>
+              </label>
+              <input type="file" id="foto" onChange={(e) => setFile(e.target.files[0])} />
+            </div>
+            <div className="formInput">
+              <label htmlFor="desc">
+                Deskripsi <span className="wajib">*</span>
+              </label>
+              <ReactQuill className="deskBox" theme="snow" onChange={setDesc} />
+            </div>
+            <div className="formInput">
+              <label htmlFor="descSingkat">
+                Deskripsi Singkat <span className="wajib">*</span>
+              </label>
+              <ReactQuill className="deskBox" theme="snow" onChange={setDescSingkat} />
+            </div>
+            <div className="formInput">
+              <label htmlFor="kategori">
+                Kategori <span className="wajib">*</span>
+              </label>
+              <select id="kategori" onChange={(e) => setCategory(e.target.value)}>
+                <option>Pilih Kategori</option>
+                <option value="politik">politik</option>
+                <option value="ekonomi">ekonomi</option>
+                <option value="sosial">sosial</option>
+                <option value="budaya">budaya</option>
+                <option value="teknologi">teknologi</option>
+                <option value="hiburan">hiburan</option>
+              </select>
+            </div>
+            <div className="formButton">
+              <button type="submit">Tambah</button>
+              <button type="button" onClick={() => listNews()}>
+                Batal
+              </button>
+            </div>
+          </form>
+        )}
       </div>
     </div>
   );
