7535e9d62cc22681cae6f7a8c6f5d269b1e2a7a3
zildanpandarusm <zildanmarginata@gmail.com>
2024-08-26 21:21:43 +0700

    add logout btn

--- frontend/src/components/admincomponents/header.js
+++ frontend/src/components/admincomponents/header.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { UserLogout, reset } from '../../features/userauth';
 import '../css/AdminStyle.css';
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    dispatch(UserLogout());
+    dispatch(reset());
+    navigate('/admin/login');
+  };
   return (
     <div className="header">
       <h1>Desa Kajoran</h1>
-      <button>
+      <button onClick={handleLogout}>
         <FontAwesomeIcon className="icon" icon={faRightFromBracket} />
       </button>
     </div>
